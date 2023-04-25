0a7c55c0cfe9356701d058426715c8b7f4881384
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-04-25 21:41:55 +0200

    project setup and database configuration

---
+++ asen/common/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from asen.common.models import (
+    Gender
+)
+
+
+# Register your models here.
+admin.site.register(Gender)
---
+++ asen/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from asen.users.models import User
+from asen.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()

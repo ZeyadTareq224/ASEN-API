7e729220e2529e4183eddece08aadd4f55c12c9d
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-04-27 14:12:37 +0200

    change disable_patient action to toggle patient_status

--- asen/patients/api/views.py
+++ asen/patients/api/views.py
@@ -4,15 +4,14 @@ from rest_framework.response import Response
 from asen.patients.models import Patient
 from asen.patients.api.serializers import PatientSerializer
 
+
 class PatientViewSet(ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
-    
+
     @action(detail=True, methods=["get"])
-    def disable_patient(self, request, pk=None):
+    def toggle_patient_status(self, request, pk=None):
         instance = self.get_object()
-        if instance.status == "active":
-            instance.status = "disabled"
-            instance.save()
-            return Response({"message": f"Patient status is now {instance.status}."})
-        return Response({"message": "Patient already disabled."})
+        instance.status = "disabled" if instance.status == "active" else "active"
+        instance.saved()
+        return Response({"meesage": f"You changed the patient status to be {instance.status}"})

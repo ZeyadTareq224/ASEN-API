c05a9bb80214e1eb2e9784c282fb3b6b85e5a90c
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-04-27 14:52:10 +0200

    link patient model with user model

--- asen/patients/models.py
+++ asen/patients/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 from asen.common.models import Gender
+
+
+User = get_user_model()
+
 
 # Create your models here.
 class Patient(models.Model):
-    PATIENT_STATUS = [('active', 'Active'), ('disabled', 'Disabled')]
-    
+    PATIENT_STATUS = [("active", "Active"), ("disabled", "Disabled")]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     status = models.CharField(max_length=20, choices=PATIENT_STATUS)

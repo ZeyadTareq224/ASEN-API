6586554d0de96a992b4be251fa81c914fd6247f7
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-04-28 19:15:22 +0300

    added swagger config in seperate file and move the path to main urls

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,9 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
+from .swagger import schema_view
 
 urlpatterns = [
-    path("swagger/", include("users.urls")),
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
@@ -17,7 +17,8 @@ urlpatterns = [
     # User management
     path("users/", include("asen.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
+    # Swagger API doc urls
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS

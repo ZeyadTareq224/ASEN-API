93b9fe643d04747bd9206e2d13e9dba6fff49161
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-04-28 18:17:03 +0300

    use the UserSerializer instead of the SimpleUserSerializer

--- asen/patients/api/serializers.py
+++ asen/patients/api/serializers.py
@@ -6,14 +6,8 @@ from asen.patients.models import Patient
 from asen.users.api.serializers import UserSerializer
 
 
-class SimpleUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ["id", "last_login", "date_joined", "name", "email"]
-
-
 class PatientSerializer(serializers.ModelSerializer):
-    user = SimpleUserSerializer()
+    user = UserSerializer()
 
     class Meta:
         model = Patient

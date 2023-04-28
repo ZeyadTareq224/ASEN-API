8940d04628f85c9357080947f8bcecc78f8d51e5
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-04-28 20:57:50 +0300

    fix error of swagger doc not loading

--- asen/users/api/serializers.py
+++ asen/users/api/serializers.py
@@ -6,6 +6,7 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        ref_name = "CustomUserSerializer"
         model = User
         fields = ["id", "name", "url"]
 

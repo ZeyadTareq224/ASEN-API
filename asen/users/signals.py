d7d1887f5b36c697ef253013989da6d6a9b38fe6
ZeyadTareq224 <ziyadtr101@gmail.com>
2023-05-04 12:08:56 +0300

    Added a post_save signal on the user creation to send welcome email

--- asen/users/signals.py
+++ asen/users/signals.py
@@ -3,9 +3,19 @@ from django.db.models.signals import post_save
 
 from asen.patients.models import Patient
 from django.contrib.auth import get_user_model
+from .tasks import send_welcome_email
 
 
-@receiver(post_save, sender=get_user_model())
-def create_patient(sender, instance, created, **kwargs):
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_patient_on_user_creation(sender, instance, created, **kwargs):
     if created:
         Patient.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def send_welcome_email_on_user_creation(sender, instance, created, **kwargs):
+    if created:
+        send_welcome_email.delay(instance.email)
